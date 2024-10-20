2ee81472d5ae8bd9d4539aafc505163105ab90d6
mosidd-01 <s760132@gmail.com>
2024-10-19 21:19:38 -0500

    Initial Commit

---
+++ Hadith App/Hadith_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hadith_AppApp.swift
+//  Hadith App
+//
+//  Created by Muhammad Siddiqui on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Hadith_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
